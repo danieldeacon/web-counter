5aff7ef5a3af3db0625ba6e470ab7130f444bc00
danieldeacon <daniel.deacon@umuzi.org>
2024-11-07 09:49:27 +0200

    Webcounter CD - Attempt 5 - Added conditional logic to resource creation.

--- main.tf
+++ main.tf
@@ -3,22 +3,14 @@ provider "aws" {
   region = "af-south-1"
 }
 
-variable "ec2_private_key" {
+variable "ec2_key_private_key" {
   description = "The name of the EC2 key pair to use"
   type        = string
 }
 
-# Security Group Check (Data Source)
-data "aws_security_group" "existing_sg" {
-  filter {
-    name   = "group-name"
-    values = ["ci-cd-webcounter-access"]
-  }
-}
-
-# Security Group Creation (Only if not found)
+/*
+# Security Group
 resource "aws_security_group" "webcounter_sg" {
-  count       = length(data.aws_security_group.existing_sg.ids) == 0 ? 1 : 0
   name        = "ci-cd-webcounter-access"
   description = "Allow HTTP, SSH, and PostgreSQL access"
   vpc_id      = "vpc-00e5757e0834b17c4"
@@ -51,26 +43,16 @@ resource "aws_security_group" "webcounter_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+*/
 
-# Check for existing EC2 Instance (Data Source)
-data "aws_instance" "existing_instance" {
-  filter {
-    name   = "tag:Name"
-    values = ["ci-cd-webcounter"]
-  }
-}
-
-# EC2 Instance Creation (Only if not found)
+# EC2 Instance
 resource "aws_instance" "ci-cd-webcounter" {
-  count         = length(data.aws_instance.existing_instance.ids) == 0 ? 1 : 0
-  ami           = "ami-00a3e6c53c910eba6"
+  ami           = "ami-0f256846cac23da94"
   instance_type = "t3.micro"
   key_name      = var.ec2_private_key
   subnet_id     = "subnet-034e3d26503920841"
 
-  vpc_security_group_ids = length(data.aws_security_group.existing_sg.ids) > 0 ?
-    [data.aws_security_group.existing_sg.ids[0]] : 
-    [aws_security_group.webcounter_sg[0].id]
+  vpc_security_group_ids = [aws_security_group.webcounter_sg.id]
 
   tags = {
     Name = "ci-cd-webcounter"
@@ -80,7 +62,5 @@ resource "aws_instance" "ci-cd-webcounter" {
 # Output EC2 Public IP
 output "ec2_public_ip" {
   description = "The public IP of the EC2 instance"
-  value = length(data.aws_instance.existing_instance.ids) > 0 ? 
-          data.aws_instance.existing_instance[0].public_ip : 
-          aws_instance.ci-cd-webcounter[0].public_ip
+  value       = aws_instance.ci-cd-webcounter.public_ip
 }
