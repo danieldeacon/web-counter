1045ceae6e69ba430bfb458a3c5c930c57d68aa7
danieldeacon <daniel.deacon@umuzi.org>
2024-11-07 14:42:28 +0200

    Refactoring

--- tests.py
+++ tests.py
@@ -1,11 +1,8 @@
 import os
 
-def test_example():
-    assert os.getenv('TEST') == 'OK', "TEST variable should be OK"
+test = os.getenv('TEST')
 
 if __name__ == '__main__':
-    test = os.getenv('TEST')
-
     if test == 'OK':
         print("All tests passed!")
 
