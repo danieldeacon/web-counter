f08bc9ada5ef076726670bd45f7fc710e0783c1e
danieldeacon <daniel.deacon@umuzi.org>
2024-11-14 15:36:43 +0200

    Webcounter CD - Attempt 7 - Debug IP address.

--- setup.sh
+++ setup.sh
@@ -3,22 +3,20 @@
 LOG_FILE=~/install_log.txt
 
 echo "Starting Docker installation..." | tee -a $LOG_FILE
-sudo apt-get update -y >> $LOG_FILE 2>&1
-sudo apt-get install -y docker.io docker-compose >> $LOG_FILE 2>&1
+sudo apt-get update -v -y >> $LOG_FILE 2>&1
+sudo apt-get install -v -y docker.io docker-compose >> $LOG_FILE 2>&1
 
-echo "Replacing placeholders in docker-compose.yaml..." | tee -a $LOG_FILE
-sed -i "s|\${DOCKER_USERNAME}|${DOCKER_USERNAME}|g" ~/docker-compose.yaml >> $LOG_FILE 2>&1
-sed -i "s|\${RELEASE_TAG}|${RELEASE_TAG}|g" ~/docker-compose.yaml >> $LOG_FILE 2>&1
+echo "image: ${DOCKER_USERNAME}/ci-cd-webcounter-backend:${RELEASE_TAG}" | tee -a $LOG_FILE
+echo "image: ${DOCKER_USERNAME}/ci-cd-webcounter-frontend:${RELEASE_TAG}" | tee -a $LOG_FILE
 
 echo "Running Docker Compose..." | tee -a $LOG_FILE
 sudo docker-compose -f ~/docker-compose.yaml up -d >> $LOG_FILE 2>&1
 
-echo "Waiting for containers to be ready..." | tee -a $LOG_FILE
-until curl -s --head --request GET http://localhost:80 | grep "200 OK" > /dev/null; do
-  echo "Waiting for the application to be available on port 80..." | tee -a $LOG_FILE
-  sleep 5
-done
-
-echo "Application is up and running on port 80!" | tee -a $LOG_FILE
+echo "Checking if the application is up on port 80..." | tee -a $LOG_FILE
+if curl --output /dev/null --silent --head --fail http://localhost:80/api/pressed; then
+    echo "Application is up and running on port 80!" | tee -a $LOG_FILE
+else
+    echo "Application failed to start. Check logs for details." | tee -a $LOG_FILE
+fi
 
 cat $LOG_FILE
