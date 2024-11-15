6cec5dcf97aae6c91ce08b1ff6e9b9361b763486
danieldeacon <daniel.deacon@umuzi.org>
2024-11-15 20:46:43 +0200

    Refactoring workflow

--- setup.sh
+++ setup.sh
@@ -11,14 +11,29 @@ else
 fi
 
 echo "-----INSTALLING DOCKER-----" | tee -a $LOG_FILE
-sudo apt-get update -v -y >> $LOG_FILE 2>&1
-sudo apt-get install -v -y docker.io docker-compose >> $LOG_FILE 2>&1
 
-echo "Using backend image: ${DOCKER_USERNAME}/ci-cd-webcounter-backend:${RELEASE_TAG}" | tee -a $LOG_FILE
-echo "Using frontend image: ${DOCKER_USERNAME}/ci-cd-webcounter-frontend:${RELEASE_TAG}" | tee -a $LOG_FILE
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do 
+sudo apt-get remove $pkg -y; 
+done | tee -a $LOG_FILE
+
+sudo apt-get update -y | tee -a $LOG_FILE
+sudo apt-get install ca-certificates curl -y | tee -a $LOG_FILE
+sudo install -m 0755 -d /etc/apt/keyrings | tee -a $LOG_FILE
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc | tee -a $LOG_FILE
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null | tee -a $LOG_FILE
+sudo apt-get update  -y | tee -a $LOG_FILE
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y | tee -a $LOG_FILE
 
 echo "-----COMPOSING CONTAINERS-----" | tee -a $LOG_FILE
-sudo docker-compose -f ~/docker-compose.yaml up -d >> $LOG_FILE 2>&1
+echo "Using backend image: ${DOCKER_USERNAME}/ci-cd-webcounter-backend:${RELEASE_TAG}" | tee -a $LOG_FILE
+echo "Using frontend image: ${DOCKER_USERNAME}/ci-cd-webcounter-frontend:${RELEASE_TAG}" | tee -a $LOG_FILE
+sudo docker-compose -f /home/ubuntu/docker-compose.yaml up -d >> $LOG_FILE 2>&1
 
 echo "Waiting for containers to initialize..." | tee -a $LOG_FILE
 sleep 60
